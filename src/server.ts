c5f4bd32768cea8c7ce6f7863df625519a900aac
Ifenna-Pascal <ifennamonanu70@gmail.com>
2022-10-24 17:16:21 +0100

    created dist folder

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,16 @@
-import server from './app';
+import server from "./app";
 
-server.listen(process.env.PORT as string, () => {
-    console.log('server is listening');
-})
-
-// unhandled rejection, such as not throwing a catch function to a promsie
-process.on('unhandledRejection', (reason: Error | any) => {
-    console.log(`Unhandled Rejection: ${reason.message || reason}`);
-
-    throw new Error(reason.message || reason);
+server.listen(process.env.PORT || 9000, () => {
+  console.log("server is listening");
 });
 
-process.on('uncaughtException', (error: Error) => {
-    console.log(`Uncaught Exception: ${error.message}`, error);
+// unhandled rejection, such as not throwing a catch function to a promsie
+process.on("unhandledRejection", (reason: Error | any) => {
+  console.log(`Unhandled Rejection: ${reason.message || reason}`);
 
+  throw new Error(reason.message || reason);
+});
+
+process.on("uncaughtException", (error: Error) => {
+  console.log(`Uncaught Exception: ${error.message}`, error);
 });
